e46b5dfc040349be1929e9f896610c84b012e776
Michael Bruno <brunomike254@gmail.com>
2022-03-12 20:12:38 +0300

    Implemented the rows,currently working on the banner

---
+++ src/components/row/Row.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import axios from "../../axios";
+import "./Row.css";
+const IMG_BASE_URL = "https://image.tmdb.org/t/p/original/";
+
+const Row = ({ title, fetchUrl, isLargeRow }) => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const request = await axios.get(fetchUrl);
+      //console.log(request.data.results);
+      setMovies(request.data.results);
+      return request;
+    };
+    fetchData();
+  }, [fetchUrl]);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${IMG_BASE_URL}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Row;
